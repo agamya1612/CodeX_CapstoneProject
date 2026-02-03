b1cfdf8706cb68c9dd09712b983aafbdf32e6c63
Agamya <46354525+agamya1612@users.noreply.github.com>
2026-02-03 16:47:02 +0530

    Delete app directory

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from producer import submit_code
-
-if __name__ == "__main__":
-    payload = {
-        "language": "python",
-        "code": "print(input())",
-        "input": "Hello Kafka"
-    }
-    submit_code(payload)
